241805c50b7f0cbfca10f289d1e5a1e5762abd16
Fernando Neirot <fernando.neirot@hotmail.com>
2025-01-06 02:43:07 -0300

    agrego ruteo de varias paginas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/home/presentation";
 import LoginPage from "./pages/login/presentation";
 import HeaderComponent from "./shared/components/header";
+import DoctorPage from "./pages/doctor/presentation";
+import PacientPage from "./pages/pacient/presentation";
+import CalendarPage from "./pages/calendar/presentation";
+import AdministrationPage from "./pages/admin/presentation";
 function App() {
   return (
     <div className="w-full bg-black">
@@ -11,6 +15,11 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="login" element={<LoginPage />} />
+          {/* Estas pagina deben ser protegidas */}
+          <Route path="doctor" element={<DoctorPage />} />
+          <Route path="admin" element={<AdministrationPage />} />
+          <Route path="pacient" element={<PacientPage />} />
+          <Route path="calendar" element={<CalendarPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/calendar/presentation/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CalendarPage = () => {
+  return (
+    <div className='bg-orange-300'>CalendarPage</div>
+  )
+}
+
+export default CalendarPage
